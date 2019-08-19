5ecf059cd921b809cea6f539d7e09216eb47a295
Max Cervantes Dominguez <max.cervantes@nearshoretechnology.com>
2019-08-19 17:02:31 -0500

    remove unnecesary stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http'
 
 import { FilterModule } from '../filter/filter.module';
-import { ConfigurableTableModule } from '../configurable-table/configurable-table.module'
 
 import { AppComponent } from './app.component';
 import { FilterFieldsService } from './filter-fields.service';
@@ -12,7 +11,7 @@ import { FilterFieldsService } from './filter-fields.service';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, FilterModule, HttpModule, ConfigurableTableModule],
+  imports:      [ BrowserModule, FormsModule, FilterModule, HttpModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [FilterFieldsService]
